dfd8ae3e322fe6ac7ab44f17d1fa854370a8e162
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-02 14:49:36 -0400

    Create udemyday2datatypes.js

---
+++ udemyday2datatypes.js
@@ -0,0 +1,13 @@
+/*1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console */
+let isisland=true;
+let country;
+let population=1000;
+let countryName="usa";
+console.log(typeof(isisland));
+console.log(typeof(country));
+console.log(typeof(countryName))
+console.log(typeof(population));
