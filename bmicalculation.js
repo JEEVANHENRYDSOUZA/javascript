ae20836f4358cc367af602cd8c7715aeaa7b5e2c
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-02 15:03:35 -0400

    Create bmicalculation.js

---
+++ bmicalculation.js
@@ -0,0 +1,9 @@
+let johnMass=92;
+let marksMass=78;
+let johnHeight=1.95;
+let markHeight=1.69;
+let bmiJohn=(johnMass)/(johnHeight)**2;
+let bmiMark=marksMass/(markHeight)**2;
+console.log(bmiJohn);
+console.log(bmiMark);
+console.log(bmiJohn > bmiMark)
