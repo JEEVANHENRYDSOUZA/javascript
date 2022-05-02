ee062e6714f4e1045765d63ca4868d8e13b648de
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-02 14:52:40 -0400

    Create typesofvariables.js

---
+++ typesofvariables.js
@@ -0,0 +1,12 @@
+const country="india";
+const continent="asia";
+let isisland=false;
+let population=1000;
+let countryName="usa";
+console.log(typeof(isisland));
+console.log(typeof(country));
+console.log(typeof(countryName))
+console.log(typeof(population));
+console.log(country)
+country=10;///never change a constant this will give error
+//let vs var will be discussed later on during the course
