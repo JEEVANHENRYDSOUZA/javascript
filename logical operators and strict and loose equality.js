588e019658ba4595a1d40d5f995da66d3a1a5996
Jeevan Henry Dsouza <77383748+JEEVANHENRYDSOUZA@users.noreply.github.com>
2022-05-05 20:41:21 -0400

    Create logical operators and strict and loose equality.js

---
+++ logical operators and strict and loose equality.js
@@ -0,0 +1,21 @@
+
+//strict and loose equality
+//strict equality does not perform type coercion 
+//loose equality will perform type conversion on it own
+
+
+
+
+
+//example
+
+console.log(18=='18');//will be true
+console.log(18===18);//will also be true
+
+
+//conclusion always use === to check equality and not ==
+//logical operator and---&&
+//or---||
+//not --!
+
+
